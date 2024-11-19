fecd4e5edb4eee1284dbc5d68da147dd89ae86a5
usdeveloper <usdeveloper@gmail.com>
2024-11-19 18:00:56 +0000

    first

--- renu.tf
+++ renu.tf
@@ -1,1 +1,9 @@
-Hi rneuka, welcome}
+resource "aws_instance" "abc" {
+  ami = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "linux"
+  tags = {
+    Name = "scrip-terraform"
+  }
+}
